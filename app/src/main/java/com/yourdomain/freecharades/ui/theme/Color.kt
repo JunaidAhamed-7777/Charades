dccc4d9d7ada279da4a06064cc389ab01bdd59b4
Junaid Ahamed <junuhammad2@gmail.com>
2026-08-20 16:56:57 +0530

    define colors

---
+++ app/src/main/java/com/yourdomain/freecharades/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.yourdomain.freecharades.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val DarkBackground = Color(0xFF121212)
+val DarkSurface = Color(0xFF1E1E1E)
+val DarkSurfaceElevated = Color(0xFF252525)
+val AccentTeal = Color(0xFF4DB6AC)
+val SoftIndigo = Color(0xFF7986CB)
+val TextPrimary = Color(0xFFEDEDED)
+val TextSecondary = Color(0xFFB0B0B0)
